0b1994800bdd49171fe09c30798b3b368505e808
gericlois <gericlois.casugay@lorma.edu>
2025-02-12 22:22:41 +0800

    Index changed

--- pages/index.php
+++ pages/index.php
@@ -12,13 +12,13 @@
             <div class="owl-carousel owl-banner">
                 <div class="item item-1">
                     <div class="header-text">
-                        <h2 class="one">Welcome to Self-Transforming Brain!</h2>
-                        <p class="one"><span class="highlighted-text">I’M SERVET EGE</span>,a Certified Rapid Transformational Hypnotherapist, RTT Therapist and Qualified Brain & Finance Coach. I work with Women and Students to help them to discover and unlock the power of their mind and transform their lives. My mission is simple: to empower Women and Students to take control of their thoughts, mindset, and habits so they can live a life that’s not only fulfilling but transformative. Together, we’ll clear emotional blocks, uncover the root causes of the struggles, and create a pathway for lasting growth and success.
+                        <h2 class="one">TRY!</h2>
+                        <p class="one"><span class="highlighted-text">I’M SERVET EGE</span>, a Certified Rapid Transformational Hypnotherapist, RTT Therapist and Qualified Brain & Finance Coach. I work with Women and Students to help them to discover and unlock the power of their mind and transform their lives. My mission is simple: to empower Women and Students to take control of their thoughts, mindset, and habits so they can live a life that’s not only fulfilling but transformative. Together, we’ll clear emotional blocks, uncover the root causes of the struggles, and create a pathway for lasting growth and success.
 
                         </p>
                         <h3 class="one">I’m thrilled to have you here and look forward to being a part of your transformation!
                         </h3>
-                        <br><br><br><br>
+                        <br><br><br>
                         <span class="category one">BOOK A FREE CALL</span> <span class="transparent">DISCOVER MORE</span>
                         <p class="one">LONDON, ESSEX – WORLDWIDE VIA ZOOM</p>
                     </div>
@@ -26,33 +26,31 @@
                 <div class="item item-2">
                     <div class="header-text">
                         <br>
-                        <br>
-                        <br>
                         <h2 class="two">TRANSFORM YOUR MIND, <br>TRANSFORM YOUR LIFE.</h2>
                         <p class="two">RTT – Rapid Transformational Therapy is totally unique, powerful and pioneering therapy based
                             on Neuroscience that offers fast and effective results by combining the principles and techniques
                             of Hypnotherapy, Psychotherapy, NLP - Neurolinguistics Programming and CBT - Cognitive
                             Behavioural Therapy.
                         </p>
-                        <br><br><br><br><br><br>
+                        <br><br><br><br>
                         <span class="category two">BOOK A FREE CALL</span> <span class="transparent">DISCOVER MORE</span>
-                        <p class="two">LONDON, ESSEX – WORLDWIDE VIA ZOOM</p>
+                        <p class="twodown">LONDON, ESSEX – WORLDWIDE VIA ZOOM</p>
                     </div>
                 </div>
                 <div class="item item-3">
-                    <div class="header-text"><br><br><br>
+                    <div class="header-text"><br>
                         <p class="three">BRAIN AND SELF-IMAGE COACHING</p>
                         <h2 class="three">Are You Ready to Break Free from the Past, Step Into Your Power and New Self?</h2>
-                        <br><br><br><br><br><br><br><br><br>
+                        <br><br><br><br>
                         <span class="category three">BOOK A FREE CALL</span> <span class="transparent">DISCOVER MORE</span>
                         <p class="three">LONDON, ESSEX – WORLDWIDE VIA ZOOM</p>
                     </div>
                 </div>
                 <div class="item item-4">
-                    <div class="header-text"><br><br><br>
+                    <div class="header-text"><br>
                         <p class="four">FINANCE AND BUSINESS COACHING</p>
                         <h2 class="four">SUCCESS BEGINS WITH A GROWTH MINDSET. LET’S CREATE THE LIFE AND BUSINESS YOU DREAM OF!</h2>
-                        <br><br><br><br><br><br><br><br><br>
+                        <br><br><br><br>
                         <span class="category four">BOOK A FREE CALL</span> <span class="transparent">DISCOVER MORE</span>
                         <p class="four">LONDON, ESSEX – WORLDWIDE VIA ZOOM</p>
                     </div>
