b0ef9b21b0f54dd81ce42f26301b06ccefe7ea85
gericlois <gericlois.casugay@lorma.edu>
2025-03-06 02:08:26 +0800

    Added Finance Page

---
+++ pages/finance.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<lang="en">
+
+    <?php include "includes/head.php" ?>
+
+    <?php include "includes/header.php" ?>
+
+    <div class="main-banner">
+        <div class="finance section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12">
+                        <div class="header-text">
+                            <div class="header">
+                                <img src="../assets/images/includes/finance_head.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="imageright">
+                            <img src="../assets/images/includes/finance_portrait.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="context">
+                            <p class="one"><span class="highlighted-text">WELCOME TO A TRANSFORMATIVE JOURNEY</span>
+                                where your
+                                vision for a successful, thriving business comes to life. Whether you're a startup
+                                founder, a small business owner, or an aspiring entrepreneur, business coaching is
+                                your gateway to understanding the fundamental principles that drive profitability,
+                                growth, and sustainability. <span class="highlighted-text">MY GOAL is SIMPLE:</span> to equip you with the strategic
+                                insights, tools, and mindset shifts that will empower you to build a profitable
+                                business while preserving your work-life harmony.<br><br>
+
+                                <span class="highlighted-text">I Offer Specialized Finance and Business Coaching</span> designed for <span class="highlighted-text">WOMEN</span> who are
+                                business owners, solo entrepreneurs, sole traders, or those aspiring to start their
+                                own ventures. With a deep understanding of the unique challenges women face in
+                                business, I provide personalized guidance to help you gain confidence in managing
+                                finances, scaling your business, and achieving long-term success.<br><br>
+
+                                Whether you're navigating the early stages of a startup or seeking to expand an
+                                existing business, I work with you to create actionable plans and strategies to
+                                drive growth, improve financial literacy, and develop the discipline needed to
+                                thrive.<br><br>
+
+                                In addition to working with <span class="highlighted-text">ENTREPRENEURS,</span> I am also passionate about helping
+                                <span class="highlighted-text">University Students and Recent Graduates—Both Undergraduates and Postgraduates</span>—who
+                                are looking to start their own businesses or grow family-run enterprises. Through
+                                tailored finance and business coaching, I guide these individuals through the
+                                process of turning their business ideas into reality, overcoming obstacles, and
+                                finding sustainable growth strategies.
+
+                            </p>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-lg-6">
+                        <div class="context">
+                            <p class="two">My coaching helps students and graduates
+                                build a strong foundation for their future ventures, whether they’re entering the
+                                world of business for the first time or continuing the legacy of a family
+                                business.<br>
+
+                                With 28 years of experience in the City of London, having held senior roles such as
+                                Finance Director, Head of Finance, Financial and Management Accountant, and Financial
+                                and Business Analyst, I take great satisfaction in working closely with company <span class="highlighted-text">Board
+                                Members, CEOs, CFOs, Business Owners, and Founders</span> to help them grow their businesses
+                                profitably. I offer a personalized, streamlined service that simplifies financial
+                                jargon, enabling clients and Board members to better understand their business numbers
+                                and maintain control over their finances.<br>
+
+
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="imageleft">
+                            <img src="../assets/images/includes/finance_image.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12">
+                        <div class="context">
+                            <p class="three"><br>I specialize in supporting <span class="highlighted-text">High-growth Businesses, SMEs, Social
+                                Enterprises,
+                                and Charities,</span> ensuring that they remain profitable and meet their targets.
+                                Additionally, I enjoy interpreting the story behind the numbers, helping Business
+                                owners, CEOs, Budget holders, and Board members to make informed decisions that not only
+                                boost their profitability but also enhance tax efficiency of their businesses.
+                                Understanding numbers is more than just a skill; it’s a powerful tool that opens doors
+                                to limitless possibilities.<br><br>
+
+                                We’ve all heard it: running a business means endless hustle, burnout, and sacrificing
+                                your personal life. And that myth about having to choose between building a business or
+                                enjoying life? It’s simply not true.<br><br>
+
+                                From my professional experience working with CEOs, Business owners, and Founders, I’ve
+                                learned that building a thriving business and achieving financial freedom isn’t about
+                                overworking. It’s about mastering business finances and making intentional, data-driven
+                                decisions. It’s also crucial to understand and analyse the market and industry in which
+                                your business operates.<br><br>
+
+                                This is where I come in. <span class="highlighted-text">As a Financial and Business Coach,</span> I can help to refine your
+                                business’s pricing strategies, review financial reports, and eliminate unprofitable
+                                services. Ultimately, this will enable you to create and grow the business you've always
+                                dreamed of. Sometimes, this means making tough decisions, like letting go of in-demand
+                                services that aren't financially viable. However, every business decision and strategy
+                                are based on the numbers and aligned with your goals.<br><br>
+
+                                <span class="highlighted-text">Every successful business is built on a well-defined strategy. It’s not just about
+                                working hard; it’s about working smart.</span> Together, we'll explore your business's unique
+                                strengths, identify areas for improvement, and develop a strategic roadmap to maximize
+                                growth.<br><br>
+
+                                The <span class="highlighted-text">Market Research</span> is very important for businesses to survive and thrive in the
+                                industry that they are in. Understanding your target audience and industry landscape is
+                                essential for your business. <span class="highlighted-text">As a Finance and Business Coach,</span> I will guide you through
+                                conducting comprehensive market research to stay ahead of your business competitors.
+                                From pricing strategies to expanding product lines, I will help you to identify scalable
+                                growth opportunities for your business that align with your long-term vision. I also
+                                discuss ways to diversify revenue streams for your business, so you’re not overly
+                                reliant on a single source of income. Also, as your Finance and Business Coach, I assist
+                                you to prepare <span class="highlighted-text">Projected Future Financial Forecasting</span> for your business’s revenue and
+                                expenses so that you can make proactive decisions.
+
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12">
+                        <div class="context">
+                            <p class="four">What I learned working in 28 years in Finance Industry is that: Our
+                                Businesses can only grow as much as we do as a person. Definitely, personal growth fuels
+                                professional success.<br>
+
+                                Mastering numbers is more than just a skill—it’s a powerful tool that unlocks the full
+                                potential of your business. If you’re feeling stuck financially, unsure of where your
+                                numbers stand, or unable to break through a plateau in your business income, remember
+                                this: you're not alone, and your past challenges don’t dictate the future of your
+                                business. The power to transform your financial journey and grow your business is in
+                                your hands.
+
+                            </p>
+
+                            <p class="five">If you're ready to break free from financial limits, change the way you
+                                think, and build a future of true success, let's take that step forward—together.
+                            </p>
+
+                            <div class="col-lg-12">
+                                <div class="button">
+                                    <a href="#" class="btn btn-primary">TRANSFORM YOUR THINKING, TRANSFORM YOUR
+                                        BUSINESS. BOOK A FREE CALL NOW.</a>
+                                </div>
+                            </div>
+
+                            <p class="six">"Shift your mindset and create and grow the business you've always dreamed
+                                of."
+                            </p>
+
+                            <p class="seven">SERVET EGE
+                            </p>
+
+
+                            <div class="col-lg-12">
+                                <div class="image">
+                                    <img src="../assets/images/includes/finance_image2.png" alt="">
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-12">
+                                <div class="footer_contact">
+                                    <img src="../assets/images/includes/finance_footer.png" alt="">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <div class="button_contact">
+                                    <a href="#" class="btn btn-primary"> EMAIL OR CONTACT ME </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    </div>
+    </div>
+
+    </Main>
+
+    <?php include "includes/footer.php" ?>
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/isotope.min.js"></script>
+    <script src="../assets/js/owl-carousel.js"></script>
+    <script src="../assets/js/counter.js"></script>
+    <script src="../assets/js/custom.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+    </html>
