421bfad5d7015914350f12c69683a095669a298e
gericlois <74848100+gericlois@users.noreply.github.com>
2025-05-28 20:30:05 +0800

    Final Rev 8 logo

--- pages/includes/head.php
+++ pages/includes/head.php
@@ -1,31 +1,33 @@
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <title>Self-TransformingBrain</title>
+  <title>Self-TransformingBrain</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" href="https://self-transformingbrain.com/assets/images/logo2.png?v=1">
+  <!-- Bootstrap core CSS -->
+  <link href="https://self-transformingbrain.com/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  
+  <!-- Favicon -->
+  <link rel="icon" href="https://self-transformingbrain.com/assets/images/logo.ico" type="image/x-icon">
 
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="https://self-transformingbrain.com/assets/css/fontawesome.css">
+  <link rel="stylesheet" href="https://self-transformingbrain.com/assets/css/main.css">
+  <link rel="stylesheet" href="https://self-transformingbrain.com/assets/css/owl.css">
+  <link rel="stylesheet" href="https://self-transformingbrain.com/assets/css/animate.css">
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="../assets/css/fontawesome.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/owl.css">
-    <link rel="stylesheet" href="../assets/css/animate.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <!-- Logo Structured Data -->
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "name": "Self-TransformingBrain",
+      "url": "https://self-transformingbrain.com",
+      "logo": "https://self-transformingbrain.com/assets/images/logo2.png"
+    }
+  </script>
 
-    <script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "Organization",
-        "name": "Self-TransformingBrain",
-        "url": "https://self-transformingbrain.com",
-        "logo": "https://self-transformingbrain.com/assets/images/logo2.png"
-      }
-    </script>
-
-  </head>
+</head>
